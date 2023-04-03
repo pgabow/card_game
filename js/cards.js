6f6ffc8b938cc1548ccf792c403855aaf13b1b8e
Paul Gabov <pgabow@gmail.com>
2023-04-03 12:36:12 +0300

    error correction

--- js/cards.js
+++ js/cards.js
@@ -101,13 +101,20 @@ const checkCard = () => (createCard[0].item === createCard[1].item ? true : fals
 const changeFace = (level = 2) => {
   const cardFace = document.querySelectorAll('.card > .card__face.card__face--back')
   cardFace.forEach(
-    (i) => (i.style.backgroundImage = `url('/img/bg${getRndInt(1, level)}.jpg')`)
+    (i) =>
+      (i.style.backgroundImage = `url('https://pgabow.github.io/card_game/img/bg${getRndInt(1,level)}.jpg')`)
   )
   if (isOver) {
-    cardFace[getRndInt(1, 15)].style.backgroundImage = `url('/img/bg9.jpg')`
-    cardFace[getRndInt(1, 15)].style.backgroundImage = `url('/img/bg10.jpg')`
+    cardFace[
+      getRndInt(1, 15)
+    ].style.backgroundImage = `url('https://pgabow.github.io/card_game/img/bg9.jpg')`
+    cardFace[
+      getRndInt(1, 15)
+    ].style.backgroundImage = `url('https://pgabow.github.io/card_game/img/bg10.jpg')`
   } else {
-    cardFace[getRndInt(1, 15)].style.backgroundImage = `url('/img/bg10.jpg')`
+    cardFace[
+      getRndInt(1, 15)
+    ].style.backgroundImage = `url('https://pgabow.github.io/card_game/img/bg10.jpg')`
   }
 }
 
